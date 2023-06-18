206e7929aa4f2f678cc19dac011c930f117907e0
Alisa Seletskaya <sealisaa@yandex.ru>
2023-06-18 09:48:55 +0300

    fix modal dialog layout

--- src/components/sections/catalog/modal/ItemModalDialog.tsx
+++ src/components/sections/catalog/modal/ItemModalDialog.tsx
@@ -11,32 +11,39 @@ export const ItemModalDialog = ({
       open={isOpen}
       onClose={() => setIsOpen(false)}>
       <div className='fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center bg-black bg-opacity-75'>
-        <Dialog.Panel className='flex w-3/4 flex-row rounded p-8 dark:bg-drBackground'>
+        <Dialog.Panel className='flex w-3/4 flex-row items-start rounded bg-drForeground p-8 dark:bg-drBackground'>
           <Image
-            className='mr-6 h-80 rounded-md object-cover object-center'
+            className='mr-6 h-80 shrink-0 grow-0 rounded-md object-cover object-center'
             width={400}
             height={400}
             src={item.imageUrl}
             alt='content'
           />
-          <div className='flex-col'>
+          <div className='relative shrink grow self-stretch'>
             <Dialog.Title className='text-xl'>
               {item.title}
             </Dialog.Title>
-            <Dialog.Description>
+            <Dialog.Description className='py-2'>
               Какое-то описание
             </Dialog.Description>
-            <button
-              className='mr-2 mt-2 rounded-md bg-drComment px-4 py-2 text-drForeground hover:bg-drComment/95'
-              onClick={() => setIsOpen(false)}>
-              Кнопка
-            </button>
-            <button
-              className='rounded-md bg-drComment px-4 py-2 text-drForeground hover:bg-drComment/95'
-              onClick={() => setIsOpen(false)}>
-              Еще кнопка
-            </button>
+            <div className='absolute bottom-0 flex flex-row'>
+              <button
+                className='mr-2 rounded-md bg-drComment px-4 py-2 text-drForeground hover:bg-drComment/95'
+                onClick={() => setIsOpen(false)}>
+                Кнопка
+              </button>
+              <button
+                className='rounded-md bg-drComment px-4 py-2 text-drForeground hover:bg-drComment/95'
+                onClick={() => setIsOpen(false)}>
+                Еще кнопка
+              </button>
+            </div>
           </div>
+          <button
+            className="shrink-0 grow-0 bg-[url('/close-btn.svg')]"
+            onClick={() => setIsOpen(false)}>
+            Закрыть
+          </button>
         </Dialog.Panel>
       </div>
     </Dialog>
